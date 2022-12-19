b4224a7b6587986e8c2f69bb7dfdb002bb1c1501
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-12-19 10:33:12 +0530

    Add files via upload

---
+++ fiborecursive.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Fibonacci(n int) int {
+
+	if n <= 1 {
+
+		return n
+
+	}
+
+	return Fibonacci(n-1) + Fibonacci(n-2)
+
+}
+
+func main() {
+	Fibonacci(10)
+	for i := 0; i < 10; i = i + 1 {
+
+		fmt.Println(Fibonacci(i))
+
+	}
+
+}
