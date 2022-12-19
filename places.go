422bc1f29744ee122e26225cb13ca84fc0c0eb35
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-12-19 10:22:46 +0530

    Add files via upload

---
+++ places.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var place1 string = "Nagaland"
+	var place2 string = "Dharamshala"
+	var place3 string = "Amritsar"
+
+	fmt.Println(place1)
+	fmt.Println(place2)
+	fmt.Println(place3)
+
+	//	fmt.Println(name1 " " )
+}
