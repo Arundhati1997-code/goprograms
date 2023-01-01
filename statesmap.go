2f07da582a1c0ed0c2c3c71c2796d6e94379a390
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2023-01-01 17:04:44 +0530

    Update statesmap.go

--- statesmap.go
+++ statesmap.go
@@ -5,13 +5,24 @@ import (
 )
 
 func main() {
-	var countryCapital = make(map[string]string)
 
-	countryCapital["India"] = "New Delhi"
-	countryCapital["Sri Lanka"] = "Colombo"
-	countryCapital["Nepal"] = "Kathmandu"
+	stateCapital := map[string]string{}
+	stateCapital["Andhra Pradesh"] = "Amaravati"
+	stateCapital["Arunachal Pradesh"] = "Itanagar"
+	stateCapital["Assam"] = "Dispur"
+	stateCapital["Bihar"] = "Patna"
+	stateCapital["Maharashtra"] = "Mumbai"
+	stateCapital["Jharkhand"] = "Ranchi"
+	stateCapital["Karnataka"] = "Bangalore"
+	stateCapital["Kerala"] = "Thiruvananthapuram"
+	stateCapital["Madhya Pradesh"] = "Bhopal"
+	stateCapital["Haryana"] = "Chandigarh"
 
-	for country, capital := range countryCapital {
-		fmt.Printf("Capital of %s is %s \n", country, capital)
+	fmt.Printf("State Capital Data Type: %T \nStateCapital: %v \n\n", stateCapital, stateCapital)
+
+	fmt.Println("Iterate over map and print state name and its capital name one by one")
+	for state, capital := range stateCapital {
+		fmt.Println(state, "->", capital)
 	}
+
 }
